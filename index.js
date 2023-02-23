2efe6ea8e48dae4d63c548d82bf7e16305288042
codegram <omondihills02@gmail.com>
2023-02-23 09:02:04 +0300

    programmed the button to display link lists when clicked

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 let close_btn=document.querySelector("#close");
 let open_btn=document.querySelector("#open");
-let menu=document.querySelector(".menu");
+var menu=document.querySelector(".menu");
+let links=document.querySelectorAll(".linked_list")
 // let close_btn=document.querySelector("#close");
 
 menu.style.backgroundColor="white";
+menu.style.display="none"
 close.addEventListener("click", closing);
 open.addEventListener("click", opening);
 
@@ -12,6 +14,8 @@ function closing(){
 }
 
 function opening(){
-
+    menu.style.display="block";
+   links.style.display="block";
+   close.style.display="inline";
 }
 
