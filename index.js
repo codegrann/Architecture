f65ace6d595d6bfaada9b689c4db96e67115a856
codegram <omondihills02@gmail.com>
2023-02-23 08:50:16 +0300

    selected the open and close buttons in the menu section

---
+++ index.js
@@ -0,0 +1,17 @@
+let close_btn=document.querySelector("#close");
+let open_btn=document.querySelector("#open");
+let menu=document.querySelector(".menu");
+// let close_btn=document.querySelector("#close");
+
+menu.style.backgroundColor="white";
+close.addEventListener("click", closing);
+open.addEventListener("click", opening);
+
+function closing(){
+
+}
+
+function opening(){
+
+}
+
