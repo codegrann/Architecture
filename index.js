09e78b6924b18ed7c29b840c2c02f2e0adef30fa
codegram <omondihills02@gmail.com>
2023-02-24 17:58:44 +0300

    added functions to toggle the buttons

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 let close_btn=document.querySelector("#close");
 let open_btn=document.querySelector("#open");
 var menu=document.querySelector(".menu");
-let links=document.querySelectorAll(".linked_list")
-// let close_btn=document.querySelector("#close");
 
-// menu.style.backgroundColor="white";
-// menu.style.display="none"
-close.addEventListener("click", closing);
-open.addEventListener("click", opening);
+
+
+close_btn.addEventListener("click", closing);
+open_btn.addEventListener("click", opening);
 
 function closing(){
-
+ open_btn.style.display="block";
+ close_btn.style.display="none";
 }
 
 function opening(){
     // menu.style.display="block";
 //    links.style.display="block";
-//    close.style.display="inline";
+    open_btn.style.display="none";
+   close_btn.style.display="block";
 }
 
